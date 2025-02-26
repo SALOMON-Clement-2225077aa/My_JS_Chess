33b64f32b23bf0fac43ff0d13e5880e739899518
Clément <clement.salomon@etu.univ-amu.fr>
2025-02-26 23:47:31 +0100

    [Done] Board with colored tiles

--- gameLogic.js
+++ gameLogic.js
@@ -1,5 +1,8 @@
-const gameBoard = document.querySelector('#gameBoard');
+// ---
+// Init variables
+// ---
 
+const gameBoard = document.querySelector('#gameBoard');
 const startBoard = [
     "B_rook", "B_Knight", "B_Bishop", "B_Queen", "B_King", "B_Bishop", "B_Knight", "B_rook",
     "B_pawn", "B_pawn"  , "B_pawn"  , "B_pawn" , "B_pawn", "B_pawn"  , "B_pawn"  , "B_pawn",
@@ -11,13 +14,43 @@ const startBoard = [
     "W_rook", "W_Knight", "W_Bishop", "W_Queen", "W_King", "W_Bishop", "W_Knight", "W_rook",
 ]
 
+// ---
+//  Create Game Board
+// ---
 
 function createBoard() {
+    var square_id = 0;
+    var isOddLine = true;
     startBoard.forEach(tile => {
         const square = document.createElement('div');
         square.classList.add("square");
+        square.setAttribute('square_id', square_id);
+        addSquareColor(square, isOddLine, square_id);
         gameBoard.append(square);
+        ++square_id;
+        if(square_id%8==0) {
+            isOddLine = !isOddLine;
+        }
     });
+}
+
+function addSquareColor(square, isOddLine, square_id) {
+    if(isOddLine) {
+        if(square_id%2==0) {
+            square.classList.add("dark-square")
+        }
+        else {
+            square.classList.add("light-square")
+        }
+    }
+    else {
+        if(square_id%2==0) {
+            square.classList.add("light-square")
+        }
+        else {
+            square.classList.add("dark-square")
+        }
+    }
 }
 
 createBoard();
