f93ff03de50890f89091deadcb31c4aae257619a
Clément <clement.salomon@etu.univ-amu.fr>
2025-02-27 12:28:41 +0100

    [WIP] Event listener (showing clicked piece infos)

--- gameLogic.js
+++ gameLogic.js
@@ -1,7 +1,6 @@
 // ---
 // Init variables
 // ---
-
 const gameBoard = document.querySelector('#gameBoard');
 const coordinatesBox = document.querySelector('#coordinatesBox');
 
@@ -16,10 +15,10 @@ const startBoard = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
 ]
 
-// ---
-//  Create Game Board
-// ---
 
+// ---
+//  Create the Game Board
+// ---
 function createBoard() {
     var square_id = 0;
     var isOddLine = true;
@@ -28,12 +27,12 @@ function createBoard() {
         square.classList.add("square");
         square.innerHTML = piece;
         if (square_id<16) {
-            square.firstChild.classList.add('black')
-            square.firstChild.firstChild.classList.add('player-black')
+            square.firstChild.classList.add('black');
+            square.firstChild.firstChild.classList.add('player-black');
         }
         if (square_id>=48) {
-            square.firstChild.classList.add('white')
-            square.firstChild.firstChild.classList.add('player-white')
+            square.firstChild.classList.add('white');
+            square.firstChild.firstChild.classList.add('player-white');
         }
         square.setAttribute('square_id', square_id);
         addSquareColor(square, isOddLine, square_id);
@@ -48,24 +47,26 @@ function createBoard() {
 function addSquareColor(square, isOddLine, square_id) {
     if(isOddLine) {
         if(square_id%2==0) {
-            square.classList.add("dark-square")
+            square.classList.add("dark-square");
         }
         else {
-            square.classList.add("light-square")
+            square.classList.add("light-square");
         }
     }
     else {
         if(square_id%2==0) {
-            square.classList.add("light-square")
+            square.classList.add("light-square");
         }
         else {
-            square.classList.add("dark-square")
+            square.classList.add("dark-square");
         }
     }
 }
 
-createBoard();
 
+// ---
+//  Create the coordinates around the board
+// ---
 function createCoordinates() {
     const abscissaCoordinates = document.getElementById('abscissa');
     const ordinateCoordinates = document.getElementById('ordinate');
@@ -73,16 +74,46 @@ function createCoordinates() {
     for (let i = 0; i < 8; i++) {
         const numLabel = document.createElement('p');
         numLabel.textContent = 8 - i;
-        numLabel.classList.add("coordinate")
+        numLabel.classList.add("coordinate");
         ordinateCoordinates.appendChild(numLabel);
     }
 
     for (let i = 0; i < 8; i++) {
         const letterLabel = document.createElement('p');
         letterLabel.textContent = String.fromCharCode(97 + i);
-        letterLabel.classList.add("coordinate")
+        letterLabel.classList.add("coordinate");
         abscissaCoordinates.appendChild(letterLabel);
     }
 }
 
+
+// ---
+//  Create the square listeners
+// ---
+function createListeners() {
+    const allSquares = document.querySelectorAll("#gameBoard .square");
+    allSquares.forEach(square => {
+        square.addEventListener("click",showPossibleMoves);
+        square.addEventListener("dragstart",dragPiece);
+    });
+}
+
+function showPossibleMoves(e) {
+    clickedSquare = e.currentTarget;
+    pieceInside = clickedSquare.firstChild;
+    console.log(clickedSquare);
+    console.log(pieceInside);
+}
+
+function dragPiece(e) {
+    // TODO
+    console.log(e.target);
+}
+
+
+// ---
+//  Call the functions
+// ---
+createBoard();
 createCoordinates();
+createListeners();
